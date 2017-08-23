aff8421b1a4c57eaf47b4e43e0894d60c37041e5
Oliver Sõro <oliver.s6ro@gmail.com>
2017-08-23 16:58:25 +0300

    Initial port Simplify.js with a similar test suite and small, medium data sets.
    https://github.com/mourner/simplify-js

---
+++ README.md
@@ -0,0 +1,4 @@
+# elm-simplify
+
+Port of Simplify.js (http://mourner.github.io/simplify-js/) in pure Elm (0.18).
+
---
+++ src/Simplify.elm
@@ -0,0 +1,201 @@
+module Simplify exposing
+  ( PixelTolerance(..)
+  , Quality(..)
+  , simplify
+  , simplifyDefault
+  )
+
+{-| Port of Simplify.js http://mourner.github.io/simplify-js/
+
+@docs PixelTolerance Quality simplify simplifyDefault
+
+-}
+
+import Dict exposing (Dict)
+
+
+{-| Simplification pixel tolerance.
+-}
+type PixelTolerance
+  = OnePixel
+  | Pixels Float
+
+{-| Simplification quality.
+    Two options:
+      * Low = Radial Distance + Ramer-Douglas-Peucker
+      * High = only Ramer-Douglas-Peucker
+-}
+type Quality
+  = Low
+  | High
+
+
+{-| Simplify with Radial Distance and/or Ramer-Douglas-Peucker
+-}
+simplify : PixelTolerance -> Quality -> List (Float, Float) -> List (Float, Float)
+simplify tolerance quality points =
+  if List.length points <= 2 then
+    points
+  else
+    let
+      sqTolerance =
+        case tolerance of
+          OnePixel -> 1
+          Pixels p -> p * p
+      newPoints =
+        case quality of
+          Low -> simplifyRadialDist points sqTolerance
+          High -> points
+    in
+      simplifyDouglasPeucker newPoints sqTolerance
+
+{-| Convenience function for the typical use case equal to:
+
+    simplify OnePixel Low
+-}
+simplifyDefault : List (Float, Float) -> List (Float, Float)
+simplifyDefault points =
+  simplify OnePixel Low points
+
+
+{-| Basic distance-based simplification
+-}
+simplifyRadialDist : List (Float, Float) -> Float -> List (Float, Float)
+simplifyRadialDist points sqTolerance =
+  let
+    initAccum =
+      points |> List.take 1
+
+    checkSqDist pointList sqt accum =
+      case (pointList, List.head accum) of
+        ([], _) -> accum
+        (x :: [], _) -> (x :: accum)
+        (x :: rest, Just p) ->
+          let
+            sqDist = squareDistance x p
+          in
+            if sqDist > sqt then
+              checkSqDist rest sqt (x :: accum)
+            else
+              checkSqDist rest sqt accum
+        (_, _) -> accum -- satisfy compiler
+  in
+    checkSqDist points sqTolerance initAccum
+      |> List.reverse
+
+{-| Square distance between 2 points.
+-}
+squareDistance : (Float, Float) -> (Float, Float) -> Float
+squareDistance (p1_x, p1_y) (p2_x, p2_y) =
+  let
+    dx = p1_x - p2_x
+    dy = p1_y - p2_y
+  in
+    (dx * dx) + (dy * dy)
+
+{-| Simplification using Ramer-Douglas-Peucker algorithm
+-}
+simplifyDouglasPeucker : List (Float, Float) -> Float -> List (Float, Float)
+simplifyDouglasPeucker points sqTolerance =
+  let
+    firstIndex = 0
+    lastIndex = (List.length points) - 1
+    inputPoints =
+      points |> List.indexedMap (,) |> Dict.fromList
+  in
+    simplifyDPStep inputPoints firstIndex lastIndex sqTolerance []
+      |> (\accum ->
+        Maybe.map2 (\fp lp -> (fp :: accum) ++ [lp]) (Dict.get firstIndex inputPoints) (Dict.get lastIndex inputPoints)
+          |> Maybe.withDefault accum
+      )
+
+{-|-}
+simplifyDPStep : Dict Int (Float, Float) -> Int -> Int -> Float -> List (Float, Float) -> List (Float, Float)
+simplifyDPStep points firstIndex lastIndex sqTolerance accum =
+  let
+    (maxSqDist, maxIndex, maxPoint) =
+      findMaxSquareSegmentDistance points firstIndex lastIndex sqTolerance
+  in
+    case maxSqDist > sqTolerance of
+      False -> accum
+      True ->
+        ( if maxIndex - firstIndex > 1 then
+            simplifyDPStep points firstIndex maxIndex sqTolerance accum
+          else
+            accum
+        )
+        ++
+        (maxPoint :: accum)
+        ++
+        ( if lastIndex - maxIndex > 1 then
+            simplifyDPStep points maxIndex lastIndex sqTolerance accum
+          else
+            accum
+        )
+
+{-|-}
+findMaxSquareSegmentDistance : Dict Int (Float, Float) -> Int -> Int -> Float -> (Float, Int, (Float, Float))
+findMaxSquareSegmentDistance points firstIndex lastIndex sqTolerance =
+  let
+    firstPoint = Dict.get firstIndex points
+    lastPoint = Dict.get lastIndex points
+    initialIndex = firstIndex + 1
+    indexes = List.range initialIndex (lastIndex - 1)
+  in
+    Maybe.map2 (\fp lp ->
+      indexes
+        |> List.foldl (\i (m, mi, mp) ->
+          points
+            |> Dict.get i
+            |> Maybe.map (\p ->
+              let
+                sqDist = squareSegmentDistance p fp lp
+              in
+                if sqDist > m then
+                  (sqDist, i, p)
+                else
+                  (m, mi, mp)
+                )
+            |> Maybe.withDefault (m, mi, mp)
+        ) (sqTolerance, initialIndex, (0, 0))
+    ) firstPoint lastPoint
+    |> Maybe.withDefault (sqTolerance, initialIndex, (0, 0))
+
+{-| Square distance from a point to a segment
+-}
+squareSegmentDistance : (Float, Float) -> (Float, Float) -> (Float, Float) -> Float
+squareSegmentDistance (p_x, p_y) (p1_x, p1_y) (p2_x, p2_y) =
+  let
+    xy = (p1_x, p1_y)
+    dx = p2_x - p1_x
+    dy = p2_y - p1_y
+    newXY =
+      if dx /= 0 || dy /= 0 then
+        let
+          (x, y) = xy
+        in
+          let
+            t = ((((p_x - x) * dx) + ((p_y - y) * dy))) / ((dx * dx) + (dy * dy))
+          in
+            if t > 1 then
+              ( p2_x
+              , p2_y
+              )
+            else
+              if t > 0 then
+                ( x + (dx * t)
+                , y + (dy * t)
+                )
+              else
+                xy
+      else
+        xy
+  in
+    let
+      (x, y) = newXY
+    in
+      let
+        dx = p_x - x
+        dy = p_y - y
+      in
+        (dx * dx) + (dy * dy)
